46e515189c4cbce309029be71cff0527c0cde250
Ilia Belyaev <144899352+ZugaduM@users.noreply.github.com>
2024-07-29 00:10:47 +0300

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -26,17 +26,17 @@ class WarehouseManager:
             proc.start()
             proc.join()
 
+if __name__ == '__main__':
+    # Создаем менеджера склада
+    manager = WarehouseManager()
 
-# Создаем менеджера склада
-manager = WarehouseManager()
+    # Множество запросов на изменение данных о складских запасах
+    requests = [("product1", "receipt", 100), ("product2", "receipt", 150),
+                ("product1", "shipment", 30), ("product3", "receipt", 200),
+                ("product2", "shipment", 50)]
 
-# Множество запросов на изменение данных о складских запасах
-requests = [("product1", "receipt", 100), ("product2", "receipt", 150),
-            ("product1", "shipment", 30), ("product3", "receipt", 200),
-            ("product2", "shipment", 50)]
+    # Запускаем обработку запросов
+    manager.run(requests)
 
-# Запускаем обработку запросов
-manager.run(requests)
-
-# Выводим обновленные данные о складских запасах
-print(dict(manager.data))
+    # Выводим обновленные данные о складских запасах
+    print(dict(manager.data))
